04f1f7ef130ed65ee34483fb15e479f7d7768318
Goro-K <69romankizil69@gmail.com>
2022-09-22 15:19:03 +0200

    test route post sauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -14,12 +14,12 @@ exports.getOneSauce = (req, res) => {
 }
 
 exports.createSauce = (req, res) => {
-    const thingSauce = JSON.parse(req.body.sauce)
-    delete thingSauce._id   // supprime l'id pour plus tard généré un nouvel id de la base de donnée
-    delete thingSauce._userId   // Supprime l'id de la personne qui à crée l'objet
+    const sauceObject = JSON.parse(req.body.sauce)
+    delete sauceObject._id   // supprime l'id pour plus tard généré un nouvel id de la base de donnée
+    delete sauceObject._userId   // Supprime l'id de la personne qui à crée l'objet
     const sauceObject = JSON.parse(req.body.sauce);
     const sauce = new Sauce({
-        ...thingSauce,
+        ...sauceObject,
         userId: req.auth.userId,
         image : `${req.protocol}://${req.get('host')}/file-upload/${req.file.filename}`
     })
