f13ca313b338e4e06e7be80c3200ba7c1654780a
Goro-K <69romankizil69@gmail.com>
2022-10-01 14:01:48 +0200

    ajout de controle de donnée

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -13,86 +13,93 @@ exports.getAllSauce = async (req, res) => {
 
 
 exports.createSauce = async (req, res) => {
-    const sauceObject = await req.body.sauce
+    const sauceObject = req.body.sauce
 
-    if(sauceObject === null || sauceObject === undefined) {
+    if(!sauceObject) {
         console.log(sauceObject)
         res.status(400).json({ message : 'Missing sauce' })
         return
     }
 
-    if(req.file === null || req.file === undefined) {
+    if(!req.file) {
         res.status(400).json({ message : 'Missing image' })
         return
     }
 
+    let sauceParsed
+
     try {
-        const sauceParsed = JSON.parse(sauceObject)
-
-        delete sauceParsed._id   // Delete the id to later generate a new id for the database
-        delete sauceParsed._userId   // Delete the id of the person who created the object
-        const sauce = new Sauce({
-            ...sauceParsed,
-            likes : 0,
-            dislikes: 0,
-            userId: req.auth.userId,
-            imageUrl : `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-        })
-
-        sauce.save()
-
-        try {
-            res.status(201).json({ message : 'Registered sauce' })
-        } catch(error){
-            console.log(error)
-            res.status(400).json({ error })
-        }
-
+        sauceParsed = JSON.parse(sauceObject)
+        
     } catch(error) {
         console.log(error)
         res.status(400).json({message : 'A field is not valid'})        // because the user goes through the api to add a sauce with a missing element
+        return
     }
+
+    delete sauceParsed._id   // Delete the id to later generate a new id for the database
+    delete sauceParsed._userId   // Delete the id of the person who created the object
+    const sauce = new Sauce({
+        ...sauceParsed,
+        likes : 0,
+        dislikes: 0,
+        userId: req.auth.userId,
+        imageUrl : `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    })
+
+    try {
+        await sauce.save()
+        res.status(201).json({ message : 'Registered sauce' })
+    } catch(error){
+        console.log(error)
+        res.status(400).json({ error })
+        return
+    }
+
 }
 
 exports.modifySauce = async (req, res) => {
-    /*
-    const sauceObject = await req.body
-    const file = await req.file
-    if(file === undefined || file === null) {
-        file {
+    // Try catch sur le parse et verifier que body n'est pas vide
+
+    let sauceObject 
+    if(req.file) {
+        if(!re)
+        sauceObject = {
             ...JSON.parse(req.body.sauce),
             imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
         }
-        res.status(400).json({ message : 'Missing image' })
     } else {
-        sauceObject
+        sauceObject = req.body
     }
-    */
-    const sauceObject = await req.file ? {
+    
+/*
+    const sauceObject = req.file ? {
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body }; // retrieves the object if it has already been transmitted
+    } : req.body ; // retrieves the object if it has already been transmitted
+*/
 
     delete sauceObject.userId // to prevent a user from creating an object in their name and then modifying it to assign it to someone else
     
     const sauce = await Sauce.findOne({_id: req.params.id})
 
-        try {
-            if(sauce.userId = req.auth.userId) {
-                sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-                try {
-                    res.status(200).json({message: 'Modified sauce !'})
-                }   catch(error) {
-                        console.log(error)
-                        res.status(400).json({error})
-                }
-            } else {
-                res.status(403).json({message: 'Unauthorized request.'})
-            }
-        }   catch(error) {
-            console.log(error)
-            res.status(400).json({error})
-        }
+    if(!sauce) {
+        res.status(404).json({message : 'Missing sauce'})
+        return
+    }
+
+    if(sauce.userId != req.auth.userId) {
+        res.status(403).json({message : 'Unauthorized request.'})
+        return
+    }
+
+    try {
+        await sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+        res.status(200).json({message: 'Modified sauce !'})
+    } catch(error) {
+        console.log(error)
+        res.status(400).json({error})
+    }
 };
 
 exports.getOneSauce = async (req, res) => {
@@ -136,13 +143,13 @@ exports.deleteAllSauce =  (req, res) => {
 
 
 exports.likeAndDislike = async (req, res) => {
-    const likeStatus = req.body.likes
+    const likeStatus = req.body.like
     const userId = req.body.userId
-    
+    console.log("toto")
     // If the user like the sauce the like is increase to one
     if(likeStatus === 1) {
-        await Sauce.updateOne({ _id: req.params.id }, { $inc:{ likes: +1 }, $push:{ usersLiked: userId } })
         try {
+            await Sauce.updateOne({ _id: req.params.id }, { $inc:{ likes: +1 }, $push:{ usersLiked: userId } })
             res.status(200).json({message: 'Like has been increased'})
         } catch {
             console.log(error)
@@ -151,16 +158,16 @@ exports.likeAndDislike = async (req, res) => {
     }
 
     // If the user decides to remove their like or dislike
-    if(likeStatus === 0) {
-        await Sauce.updateOne({ _id: req.params.id }, { $inc:{ likes: -1 }, $pull:{ usersLikes: userId }})
+    else if(likeStatus === 0) {
         try {
+            await Sauce.updateOne({ _id: req.params.id }, { $inc:{ likes: -1 }, $pull:{ usersLikes: userId }})
             res.status(200).json({ message: 'Like has been decreased'})
         }   catch(error) {
             res.status(400).json({ error })
         }
 
-        Sauce.updateOne({ _id: req.params.id }, { $inc:{dislikes: -1 }, $pull: { usersDisliked: userId }})
         try {
+            Sauce.updateOne({ _id: req.params.id }, { $inc:{dislikes: -1 }, $pull: { usersDisliked: userId }})
             res.status(200).json({ message: 'Dislike has been decreased'})
         }   catch(error) {
             console.log(error)
@@ -169,13 +176,15 @@ exports.likeAndDislike = async (req, res) => {
     }
 
     //If user don't like the sauce  
-    if(likeStatus === -1) {
-        await Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: +1}, $push: { usersDisliked: userId}})
+    else if(likeStatus === -1) {
         try {
+            await Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: +1}, $push: { usersDisliked: userId}})
             res.status(200).json({ message: 'Dislike has been increased'})
         }   catch(error){
             console.log(error)
             res.status(400).json({ error })
         }
+    } else {
+        res.status(400).json({ message : "numero " })
     }
 }
